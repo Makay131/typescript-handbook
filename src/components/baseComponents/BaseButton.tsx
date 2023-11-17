6eb7a46d460fedd75b24c9586b5e70c5ea3c9aad
Makay131 <mahmutenesakay9@gmail.com>
2023-11-17 09:50:59 +0300

    ...

--- src/components/baseComponents/BaseButton.tsx
+++ src/components/baseComponents/BaseButton.tsx
@@ -1,7 +1,8 @@
 type BaseButtonProps = {
     variant: 'primary' | 'secondary',
     children: string
-} & Omit<React.ComponentProps<'button'>, 'children'> //we made the children to be STRING ONLY 
+} & Omit<React.ComponentProps<'button'>, 'children'> //we made the children to be STRING ONLY
+// --> also use React.ComponentPropsWithoutRef or React.ComponentPropsWithRef depending on if you have ref as a prop or not to be more specific
 export default function BaseButton({ variant, children, ...rest }: BaseButtonProps) {
   return (
     <button className={`${variant}`} {...rest}>{children}</button>
