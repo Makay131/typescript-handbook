917aa1a0cd1446e28334b1f419021924481b370e
Makay131 <mahmutenesakay9@gmail.com>
2023-11-17 10:46:44 +0300

    more on omit

--- src/components/baseComponents/BaseButton.tsx
+++ src/components/baseComponents/BaseButton.tsx
@@ -1,7 +1,17 @@
+/* OMIT
+type Admin = {
+  //all properties,
+  superpowers: true
+}
+
+type User = Omit<Admin, 'superpowers'>
+
+*/
 type BaseButtonProps = {
     variant: 'primary' | 'secondary',
     children: string
-} & Omit<React.ComponentProps<'button'>, 'children'> //we made the children to be STRING ONLY
+} & Omit<React.ComponentProps<'button'>, 'children'> //we made the children to be STRING ONLY, by extracting/omitting the children property from React.ComponentProps<'button'>
+
 // --> also use React.ComponentPropsWithoutRef or React.ComponentPropsWithRef depending on if you have ref as a prop or not to be more specific
 export default function BaseButton({ variant, children, ...rest }: BaseButtonProps) {
   return (
